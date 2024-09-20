4e4f1e13992e01798dd68674da18b39cca7a505c
boris098765 <sites-rakitin@yandex.ru>
2024-09-20 22:52:40 +0300

    Добавлена базовая вложенность Создание дочерних элементов и универсальное удаление
    Далее: Правильное редактирование эл-тов в базе

--- sidebars/left_sidebar/objects_frame.py
+++ sidebars/left_sidebar/objects_frame.py
@@ -4,9 +4,17 @@ from tkinter import ttk
 from CTkMessagebox import CTkMessagebox
 
 
+# TODO Нормальная сортировка
+# TODO Редактирование дочерних элементов
+# TODO Обработка удаления дочернего элемента
+
+
 class ObjectFrame(ctk.CTkFrame):
     def __init__(self, parent):
         self.sidebar = parent
+
+        # Формат данных:
+        # parent_id, id, name, count
         self.data = []
 
         self.table = None
@@ -29,15 +37,17 @@ class ObjectFrame(ctk.CTkFrame):
         self.setup_table()
 
     def setup_table(self):
-        self.table = ttk.Treeview(self, columns=('c1', 'c2', 'c3'), show='headings', height=5)
+        self.table = ttk.Treeview(self, columns=('c1', 'c2', 'c3', 'c4'), show='headings', height=15)
 
         # Настройка колонок
-        self.table.heading('c1', text="№")
-        self.table.heading('c2', text="Name")
-        self.table.heading('c3', text="P Count")
+        self.table.heading('c1')
+        self.table.heading('c2', text="№")
+        self.table.heading('c3', text="Name")
+        self.table.heading('c4', text="P Count")
         self.table.column('c1', width=20, stretch=False)
-        self.table.column('c2', stretch=True)
-        self.table.column('c3', width=60, stretch=False)
+        self.table.column('c2', width=20, stretch=False)
+        self.table.column('c3', stretch=True)
+        self.table.column('c4', width=60, stretch=False)
 
         self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.table.yview)
         self.table.configure(yscrollcommand=self.scrollbar.set)
@@ -55,28 +65,68 @@ class ObjectFrame(ctk.CTkFrame):
         self.edit_btn.grid(row=1, column=1, padx=5, pady=5)
         self.del_btn.grid(row=1, column=2, padx=5, pady=5)
 
+    # TODO
+    def get_item_by_id(self, parent_id, id):
+        ...
+
+    # Возвращает наследников из базы
+    def get_children(self, parent_id=0):
+        return [item for item in self.data if item[0]==parent_id]
+
+    # Удалить значение в БД вместе с дочерними
+    def delete_object(self, parent_id, id):
+        self.data = [item for item in self.data if not (item[0]==parent_id and item[1]==id)]
+        if parent_id==0:
+            self.data = [item for item in self.data if not (item[0]==id)]
+
+    def delete_items(self):
+        selected_items = self.get_selected_row()
+        if not selected_items:
+            CTkMessagebox(self, icon="cancel", title="DELETE ERROR", message="Nothing to delete!!!")
+            return
+
+        ids = []
+        for item_id in selected_items:
+            row_values = self.table.item(item_id)['values']
+            if row_values: ids.append((row_values[0], row_values[1]))
+
+        for parent_id, id in ids:
+            self.delete_object(parent_id, id)
+        self.update_table()
 
     def update_table(self):
+        # Очистка таблицы
         for i in self.table.get_children():
             self.table.delete(i)
 
-        sorted_data = sorted(self.data, key=lambda x: x[0])
-        for obj in sorted_data:
-            self.table.insert('', 'end', values=obj)
+        # Объекты
+        parents = self.get_children(0)
 
-        self.sidebar.update_menu()
+        for el in parents:
+            parent_id, point_count = el[1], el[3]
+            parent_item = self.table.insert('', 'end', values=el)
+            if point_count == 0:
+                continue
+
+            for i in range(point_count):
+                item_data = (parent_id, i+1, '', 0)
+                self.table.insert(parent_item, 'end', values=item_data)
+                self.data.append(item_data)
 
     def find_min_num(self):
         if len(self.data) == 0: return 1
-        occupied = {item[0] for item in self.data}
+
+        parents = {item for item in self.data if item[0]==0}
+        occupied = {item[1] for item in parents}
 
         first_available = 1
         while first_available in occupied:
             first_available += 1
+
         return first_available
 
     def new_item(self):
-        self.data.append((self.find_min_num(), 'name', 0))
+        self.data.append((0, self.find_min_num(), 'name', 0))
         self.update_table()
 
     def get_selected_row(self):
@@ -89,15 +139,22 @@ class ObjectFrame(ctk.CTkFrame):
         return ids
 
     def edit_item(self):
+        poboch = False
+
         selected_items = self.get_selected_row()
         if not selected_items:
             CTkMessagebox(self, icon="cancel", title="EDIT ERROR", message="Nothing to edit!!!")
             return
-        if len(selected_items) > 1: return
+        if len(selected_items) > 1:
+            CTkMessagebox(self, icon="cancel", title="EDIT ERROR", message="Too much to edit!!!")
+            return
 
         selected_id = selected_items[0]
         row_values = self.table.item(selected_id)['values']
         if not row_values: return
+
+        if row_values[0] != 0:
+            poboch = True
 
         # Окно для редактирования
         edit_window = ctk.CTkToplevel(self)
@@ -109,49 +166,33 @@ class ObjectFrame(ctk.CTkFrame):
         ctk.CTkLabel(edit_window, text="№").grid(row=0, column=0, padx=5, pady=10)
         num_entry = ctk.CTkEntry(edit_window)
         num_entry.grid(row=0, column=1, padx=10, pady=10)
-        num_entry.insert(0, row_values[0])
+        num_entry.insert(0, row_values[1])
 
         ctk.CTkLabel(edit_window, text="Name").grid(row=1, column=0, padx=5, pady=10)
         name_entry = ctk.CTkEntry(edit_window)
         name_entry.grid(row=1, column=1, padx=10, pady=10)
-        name_entry.insert(0, row_values[1])
+        name_entry.insert(0, row_values[2])
 
         ctk.CTkLabel(edit_window, text="P Count").grid(row=2, column=0, padx=5, pady=10)
         count_entry = ctk.CTkEntry(edit_window)
         count_entry.grid(row=2, column=1, padx=5, pady=10)
-        count_entry.insert(0, row_values[2])
+        count_entry.insert(0, row_values[3])
+
+        if poboch: count_entry.configure(state='disabled')
 
         def save_edit():
-            new_num = int(num_entry.get())
+            new_id = int(num_entry.get())
             new_name = name_entry.get()
             new_count = int(count_entry.get())
 
             # Обновляем данные в таблице
-            self.delete_data_item(row_values[0])
-            self.data.append((new_num, new_name, new_count))
+            # 1. Получить ids элемента в базе
+            # 2. Записать новый элемент на место старого
             self.update_table()
             edit_window.destroy()
 
         save_btn = ctk.CTkButton(edit_window, text="Save", command=save_edit)
         save_btn.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-
-    def delete_data_item(self, id_value):
-        self.data = [item for item in self.data if item[0] != id_value]
-
-    def delete_items(self):
-        selected_items = self.get_selected_row()
-        if not selected_items:
-            CTkMessagebox(self, icon="cancel", title="DELETE ERROR", message="Nothing to delete!!!")
-            return
-
-        ids = []
-        for item_id in selected_items:
-            row_values = self.table.item(item_id)['values']
-            if row_values: ids.append(row_values[0])
-
-        for id_val in ids: self.delete_data_item(id_val)
-
-        self.update_table()
 
     def get_objects(self):
         objects = [self.table.item(obj)['values'][1] for obj in self.table.get_children()]
