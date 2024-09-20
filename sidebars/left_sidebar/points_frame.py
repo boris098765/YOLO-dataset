585da4af068a7478649ec666c7d8e4671f4ed852
boris098765 <sites-rakitin@yandex.ru>
2024-09-20 14:23:51 +0300

    Добавил реакцию на удаление

--- sidebars/left_sidebar/points_frame.py
+++ sidebars/left_sidebar/points_frame.py
@@ -1,9 +1,12 @@
 import customtkinter as ctk
 from tkinter import ttk
 
+from CTkMessagebox import CTkMessagebox
+
 
 class PointsFrame(ctk.CTkFrame):
     def __init__(self, parent, table_data=None):
+        self.sidebar = parent
         self.data = table_data
         self.table = None
         self.scrollbar = None
@@ -42,9 +45,36 @@ class PointsFrame(ctk.CTkFrame):
 
         self.new_btn = ctk.CTkButton(self, text="New")
         self.edit_btn = ctk.CTkButton(self, text="Edit")
-        self.del_btn = ctk.CTkButton(self, text="Del")
+        self.del_btn = ctk.CTkButton(self, text="Del", command=self.delete_items)
 
         self.new_btn.grid(row=1, column=0, padx=5, pady=5)
         self.edit_btn.grid(row=1, column=1, padx=5, pady=5)
         self.del_btn.grid(row=1, column=2, padx=5, pady=5)
 
+    def update_table(self):
+        for i in self.table.get_children():
+            self.table.delete(i)
+
+        sorted_data = sorted(self.data, key=lambda x: x[0])
+        for obj in sorted_data:
+            self.table.insert('', 'end', values=obj)
+
+        self.sidebar.update_menu()
+
+    def get_selected_row(self):
+        selected_item = self.table.selection()
+        if not selected_item: return False
+
+        ids = []
+        for item_id in selected_item:
+            ids.append(item_id)
+        return ids
+
+    def delete_data_item(self, id_value):
+        self.data = [item for item in self.data if item[0] != id_value]
+
+    def delete_items(self):
+        selected_items = self.get_selected_row()
+        if not selected_items:
+            CTkMessagebox(self, icon="cancel", title="DELETE ERROR", message="Nothing to delete!!!")
+            return
